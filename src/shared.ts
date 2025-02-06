1867ff81418b10164cdde8760be6a5de26c90783
aseelaldallal <aseelaldallal@gmail.com>
2025-02-06 08:17:02 -0800

    stuff

--- src/shared.ts
+++ src/shared.ts
@@ -1,15 +1,16 @@
-// @@@SNIPSTART money-transfer-project-template-ts-constants
+
 export const namespace = 'default';
-export const taskQueueName = 'money-transfer';
-// @@@SNIPEND
+export const taskQueueName = 'ecom-import';
 
-// @@@SNIPSTART money-transfer-project-template-ts-shared
+export interface ImportDateSpan {
+  startDate: Date;
+  endDate: Date;
+}
 
-export type PaymentDetails = {
-  amount: number;
-  sourceAccount: string;
-  targetAccount: string;
-  referenceId: string;
-};
 
-// @@@SNIPEND
+export interface TransformedData {
+  transactions: {
+    id: string;
+    amount: number;
+  }[];
+}
--- src/workflows.ts
+++ src/workflows.ts
@@ -1,51 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // @@@SNIPSTART money-transfer-project-template-ts-workflow
 import { proxyActivities } from '@temporalio/workflow';
-import { ApplicationFailure } from '@temporalio/common';
 
 import type * as activities from './activities';
-import type { PaymentDetails } from './shared';
+import { ImportDateSpan } from './shared';
 
-export async function moneyTransfer(details: PaymentDetails): Promise<string> {
-  // Get the Activities for the Workflow and set up the Activity Options.
-  const { withdraw, deposit, refund } = proxyActivities<typeof activities>({
-    // RetryPolicy specifies how to automatically handle retries if an Activity fails.
-    retry: {
-      initialInterval: '1 second',
-      maximumInterval: '1 minute',
-      backoffCoefficient: 2,
-      maximumAttempts: 500,
-      nonRetryableErrorTypes: ['InvalidAccountError', 'InsufficientFundsError'],
-    },
-    startToCloseTimeout: '1 minute',
-  });
+const { fetchData, saveRawData, transformData, saveTransformedData, splitDateRange } = proxyActivities<typeof activities>({
+  retry: {
+    initialInterval: '1 second',
+    maximumInterval: '1 minute',
+    backoffCoefficient: 2,
+    maximumAttempts: 5,
+  },
+  // Each Activity must complete within 2 minutes or else it times out
+  startToCloseTimeout: '2 minutes',
+});
 
-  // Execute the withdraw Activity
-  let withdrawResult: string;
-  try {
-    withdrawResult = await withdraw(details);
-  } catch (withdrawErr) {
-    throw new ApplicationFailure(`Withdrawal failed. Error: ${withdrawErr}`);
-  }
 
-  //Execute the deposit Activity
-  let depositResult: string;
-  try {
-    depositResult = await deposit(details);
-  } catch (depositErr) {
-    // The deposit failed; try to refund the money.
-    let refundResult;
-    try {
-      refundResult = await refund(details);
-      throw ApplicationFailure.create({
-        message: `Failed to deposit money into account ${details.targetAccount}. Money returned to ${details.sourceAccount}. Cause: ${depositErr}.`,
-      });
-    } catch (refundErr) {
-      throw ApplicationFailure.create({
-        message: `Failed to deposit money into account ${details.targetAccount}. Money could not be returned to ${details.sourceAccount}. Cause: ${refundErr}.`,
-      });
-    }
-  }
-  return `Transfer complete (transaction IDs: ${withdrawResult}, ${depositResult})`;
+export async function importData(importDateSpan: ImportDateSpan): Promise<void> {
+
+  const { startDate, endDate } = importDateSpan;
+  const dateChunks = await splitDateRange(startDate, endDate);
+  
+  const tasks = dateChunks.map(({ chunkId, chunkStartDate, chunkEndDate }) =>
+    (async () => {
+      await executeChunkWorkflow(chunkId, chunkStartDate, chunkEndDate);
+    })()
+  );
+
+  await Promise.all(tasks);
 }
-// @@@SNIPEND
+
+async function executeChunkWorkflow(chunkId: string, startDate: Date, endDate: Date): Promise<void> {
+  const fetchedRawData = await fetchData(chunkId, startDate, endDate);
+  const savedRawData = await saveRawData(chunkId, fetchedRawData);
+  const transformedData = await transformData(chunkId, savedRawData);
+  await saveTransformedData(chunkId, transformedData);
+}
