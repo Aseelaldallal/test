db6acd63e2095786f53410d476dec0d06cf27b15
aseelaldallal <aseelaldallal@gmail.com>
2025-02-06 08:42:21 -0800

    stuff

--- src/activities.ts
+++ src/activities.ts
@@ -36,7 +36,7 @@ export const splitDateRange = (startDate: Date, endDate: Date): Chunk[] =>  {
 export const fetchData = async (id: number, startDate: Date, endDate: Date): Promise<Record<string,any>[]> => {
   console.log(`Fetching raw data for chunk: ${id}, startDate: ${startDate}, endDate: ${endDate}`);
   // Fetch raw data
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 20000));
   return [
     { id: 1, amount: 100, someRawThings: { bam: 'blippityBlop' } },
     { id: 2, amount: 200, someRawThings: { wam: 'bopbopbop' } },
@@ -47,19 +47,19 @@ export const fetchData = async (id: number, startDate: Date, endDate: Date): Pro
 export const saveRawData = async (id: number, rawData: Record<string,any>[]): Promise<Record<string,any>[]> => {
   console.log('Saving raw data for chunk:', id);
   // Save raw data
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   return rawData;
 }
 
 export const transformData = async (id: number, rawData: Record<string,any>[]): Promise<TransformedData[]> => {
   console.log('Transforming data for chunk:', id);
   // Transform raw data
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   return rawData.map(({ id, amount }) => ({ id, amount }));
 }
 
 export const saveTransformedData = async (id: number, transformedData: TransformedData[]): Promise<void> => {
   console.log('Saving transformed data for chunk:', id);
   // Save transformed data
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 10000));
 }
